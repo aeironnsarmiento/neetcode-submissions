21b919798e33dd1a1b228ad31059a9c0105bc5fa
Aeironn Sarmiento <112418386+aeironnsarmiento@users.noreply.github.com>
2026-04-23 21:55:09 -0500

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        wordMap = {}
+        
+        for word in strs:
+            key = "".join(sorted(word))
+            if key not in wordMap:
+                wordMap[key] = []
+            wordMap[key].append(word)
+
+        return list(wordMap.values())
